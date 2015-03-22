1272ad8897a9511f88c1da434747cc97132ceb7f
cnkeats <calvin.keats@gmail.com>
2015-03-22 17:51:12 -0500

    Hopefully fixed inserting

--- insert.php
+++ insert.php
@@ -1,11 +1,11 @@
-<?php$username="username";
-$password="password";$database="your_database";
+<?php$username="ckeats";
+$password="11413813";$database="cnkeats_db";
 $field1-name=$_POST['Value1'];
 $field2-name=$_POST['Value2'];
 $field3-name=$_POST['Value3'];
 $field4-name=$_POST['Value4'];
 $field5-name=$_POST['Value5'];
-mysql_connect(localhost,$username,$password);
+mysql_connect(vrbsky-oracle.cs.ua.edu,$username,$password);
 @mysql_select_db($database) or die( "Unable to select database");
-$query = "INSERT INTO tablename VALUES('','$field1-name','$field2-name',
+$query = "INSERT INTO student VALUES('','$field1-name','$field2-name',
 '$field3-name','$field4-name','$field5-name')";mysql_query($query);mysql_close();?>
