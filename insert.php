9d67fa3ada22dcf8976362a1fd9129b89b00ebd9
cnkeats <calvin.keats@gmail.com>
2015-03-22 17:55:12 -0500

    This time....?

--- insert.php
+++ insert.php
@@ -5,7 +5,7 @@ $field2-name=$_POST['Value2'];
 $field3-name=$_POST['Value3'];
 $field4-name=$_POST['Value4'];
 $field5-name=$_POST['Value5'];
-mysql_connect(vrbsky-oracle.cs.ua.edu,$username,$password);
+mysql_connect("vrbsky-oracle.cs.ua.edu",$username,$password);
 @mysql_select_db($database) or die( "Unable to select database");
 $query = "INSERT INTO student VALUES('','$field1-name','$field2-name',
 '$field3-name','$field4-name','$field5-name')";mysql_query($query);mysql_close();?>
